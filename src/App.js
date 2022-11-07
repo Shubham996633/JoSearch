254c5347b000802b258edc1995537d6e7c364caa
Shubham Maurya <risjiomaujio7654@gmail.com>
2022-11-07 21:50:49 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Routes from "./components/Routes";
 
 function App() {
+
+  const [darkTheme, setDarkTheme] = useState(false)
   return (
-    <div className="App">
-      <h1>Hey</h1>
+    <div className={darkTheme ? 'dark' : ''}>
+
+      <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
+        App
+      </div>
     </div>
   );
 }
