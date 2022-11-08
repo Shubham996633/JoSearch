fef28143351f77a5ca20881b2298c738175866a1
Shubham Maurya <risjiomaujio7654@gmail.com>
2022-11-08 14:20:09 +0530

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
 const Footer = () => {
+    const date = new Date()
+    let year = date.getFullYear()
+
     return (
-        <div>Footer</div>
+        <div className='text-center p-10 mt-10 border-t dark:boder-gray-700 border-gray-200'>{year} JoGoogle </div>
     )
 }
 
---
+++ src/contexts/ResultContextProvider.js
@@ -0,0 +1,43 @@
+import React, { createContext, useContext, useState } from 'react'
+
+const ResultContext = createContext()
+const baseUrl = 'https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI?'
+
+export const ResultContextProvider = ({ children }) => {
+    const [results, setResults] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+    const [searchTerm, setSearchTerm] = useState('')
+
+
+    const getResults = async (type) => {
+        setIsLoading(true)
+
+        const response = await fetch(`${baseUrl}${type}`,
+
+            {
+                method: 'GET',
+                headers: {
+                    "X-RapidAPI-Key": "15eef971dfmsha3593e18a102949p178aebjsn2f51369adcd5",
+                    "X-RapidAPI-Host": "contextualwebsearch-websearch-v1.p.rapidapi.com"
+                }
+            })
+
+        const data = await response.json()
+        console.log(data)
+        setResults(data)
+        setIsLoading(false)
+
+    }
+
+
+
+
+    return (
+        <ResultContext.Provider value={{ getResults, results, searchTerm, setSearchTerm, isLoading }}>
+            {children}
+        </ResultContext.Provider>
+    )
+}
+
+
+export const UseResultContext = () => useContext(ResultContext)
