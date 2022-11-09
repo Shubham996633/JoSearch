2986f44455359f716047da434b4f1fdda19f2b4d
Shubham Maurya <risjiomaujio7654@gmail.com>
2022-11-09 19:46:57 +0530

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     let year = date.getFullYear()
 
     return (
-        <div className='text-center p-10 mt-10 border-t dark:boder-gray-700 border-gray-200'>{year} JoGoogle </div>
+        <div className='text-center p-10 mt-10 border-t dark:boder-gray-700 border-gray-200 jogoogle'>{year} JoSearch - Powered By Bing </div>
     )
 }
 
